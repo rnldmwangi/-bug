644043bff1e2b546c4a79c96b2d0a3f7bd52b73c
Mwangi Wanjau <40176446+rnldmwangi@users.noreply.github.com>
2025-11-06 20:46:19 +0300

    shebang and imports
    import statements for various modules.

--- index.py
+++ index.py
@@ -1,1 +1,16 @@
-print("1st program on git hub using python");
+#!/usr/bin/env python3
+"""
+Imports
+"""
+
+import array
+import collections
+import heapq
+import itertools
+import math
+import random
+import threading
+import weakref
+from abc import ABC, abstractmethod
+from bisect import bisect_left, bisect_right
+from typing import (Any, Callable, Dict, Generic, Iterable, List, Optional, Tuple, TypeVar, Union)
